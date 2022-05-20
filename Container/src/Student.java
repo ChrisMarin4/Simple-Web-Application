943d98f3df8a851fc7a03175ed1c6b6c7f380cb4
Christos <chrismarin4@gmail.com>
2022-05-20 17:00:31 +0300

    testing

---
+++ Container/src/Student.java
@@ -0,0 +1,25 @@
+
+public class Student {
+
+	private String name;
+	private int age;
+	
+	
+	public Student (String aName, int aAge)
+	{
+		name = aName;
+		age = aAge;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	
+}
